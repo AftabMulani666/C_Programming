efd858c12d0481e2a5279afabac1760c8382ffc5
AftabMulani666 <Aftabmulani1739@gmail.com>
2024-09-09 15:39:02 +0530

    Function

---
+++ Function/001_getch.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char ch='/0';
+    printf("Enter The Character");
+    //ch=getch();
+    //ch=getche();
+    ch=getchar();
+    getch;
+    return ;
+
+}
