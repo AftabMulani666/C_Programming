2a5aaf32d5a00771b73d9e1ad220318419daf76b
AftabMulani666 <aftabmulani1739@gmail.com>
2024-09-11 21:35:37 +0530

    002_ForLoop
    For Loop

---
+++ Looping_Statements/002_ForLoop.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int num,i;
+   printf("Enter Any Number\n");
+   scanf("%d",&num);
+for ( i = 1; i <= 10; i++)
+{
+
+    printf("%d X %d = %d\n",num,i,num*i);
+}
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
