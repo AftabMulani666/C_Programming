7362e91580d0d1347bf5a05686e64af705c8d6ff
AftabMulani666 <aftabmulani1739@gmail.com>
2024-09-16 17:15:34 +0530

    Create 001_Alphabet_Range.c
    Alphabet Range Fix Input

---
+++ Character_IO/001_Alphabet_Range.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char ch='\0';
+    printf("The Alphabet Range Between F To P");
+    for(ch ='F';ch<='P';ch++)
+    {
+        printf("\n%c",ch);
+    }
+    getch();
+    return 0;
+}
