c32163dafad91b964fe5ee3121d419a7c618f71c
AftabMulani666 <aftabmulani1739@gmail.com>
2024-09-17 20:04:31 +0530

    002_Alphabet_User_Input
    Alphabet User Input

---
+++ Character_IO/002_Alphabet_User_Input.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int main()
+{
+    char ch='\0',spt='\0',ept='\0';
+    printf("\nEnter The Starting Alphabet Range:");
+    scanf("%c",&spt);
+    fflush(stdin);
+    printf("\nEnter The Ending Alphabet Range:");
+    scanf("%c",&ept);
+    for(ch =spt;ch<=ept;ch++)
+    {
+        printf("\n%c",ch);
+    }
+    getch();
+    return 0;
+}
+
