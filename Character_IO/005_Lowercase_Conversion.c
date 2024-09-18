68a9f03c5f771f34b13424b94aa8b4bc0f558ce2
AftabMulani666 <aftabmulani1739@gmail.com>
2024-09-18 22:00:49 +0530

    005_Lowercase_Conversion
    Lowercase Conversion

---
+++ Character_IO/005_Lowercase_Conversion.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char ch='\0';
+    printf("\nEnter The Alphabet To Convert It Into Lowercase:");
+    scanf("%c",&ch);
+    if(ch>='A' && ch<='Z')
+    {
+        ch=ch+32;
+        printf("The Alpahbet is Converted Into Lowercase : %c",ch);
+    }
+    else
+    {
+        printf("The Alpahbet is Not a Lowercase (Hence Not Converted)");
+    }
+    getch();
+    return 0;
+}
+
+
+
