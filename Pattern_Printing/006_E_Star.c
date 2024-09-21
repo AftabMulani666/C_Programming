57ff2f41bc9ccecfa7e6303533797a7663aa260e
AftabMulani666 <aftabmulani1739@gmail.com>
2024-09-21 15:13:27 +0530

    006_E_Star
    E Star Pattern Printing

---
+++ Pattern_Printing/006_E_Star.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int R=0,C=0,cnt=0;
+    printf("Enter Any Number");
+    scanf("%d",&cnt);
+        for(R=1;R<=cnt;R++)
+    {
+        for(C=1;C<=cnt;C++)
+        if(R==1 || C==1 || R==cnt || R==(cnt+1)/2 )
+        {
+        printf(" * ");
+        }
+        else
+        {
+          printf("   ");
+        }
+        printf("\n");
+    }
+    getch();
+    return 0;
+}
+
+
+
+
+
