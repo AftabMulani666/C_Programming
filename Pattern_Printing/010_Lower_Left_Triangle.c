a0533772ff062dd4af2aa68acd31b414280cb659
Aftab Mulani <aftabmulani1739@gmail.com>
2024-09-24 11:28:42 +0530

    010_Lower_Left_Triangle
    Lower Left Triangle Pattern Printing

---
+++ Pattern_Printing/010_Lower_Left_Triangle.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int R=0,C=0,cnt=0;
+    printf("Enter Any Number");
+    scanf("%d",&cnt);
+        for(R=1;R<=cnt;R++)
+    {
+        for(C=1;C<=cnt;C++)
+        if(C<=R)
+        {
+        printf(" * ");
+        }
+        else
+        {
+          printf("   ");
+        }
+        printf("\n");
+    }
+    getch();
+    return 0;
+}
+
+
+
+
