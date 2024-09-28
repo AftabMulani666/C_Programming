c3a44feb865b4f703898bd835ec970ea52cef869
Aftab Mulani <aftabmulani1739@gmail.com>
2024-09-28 13:27:35 +0530

    015_M_N_Value

---
+++ Conditional_Statements/015_M_N_Value.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int M;
+    printf("\nEnter The Value Of M:");
+    scanf("%d",&M);
+
+    if(M>0)
+    {
+        printf("N=1");
+    }
+    else
+    {
+        printf("N=-1");
+    }
+    getch();
+    return 0;
+
+}
+
+
