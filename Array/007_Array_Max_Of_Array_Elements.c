48fe225fc2a4f10133e8396d68b30400bc9d3ea3
Aftab Mulani <aftabmulani1739@gmail.com>
2024-10-03 16:13:15 +0530

    007_Array_Max_Of_Array_Elements

---
+++ Array/007_Array_Max_Of_Array_Elements.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#define count 8
+
+int main()
+{
+    int Arr[count]={};
+    int i=0,Max=0;
+    for(i=0;i<count;i++)
+    {
+        printf("Enter The Price Of Bill %d :",i);
+        scanf("%d",&Arr[i]);
+    }
+    Max=Arr[0];
+    for(i=0;i<count;i++)
+    {
+        if(Arr[i]>Max)
+        {
+            Max=Arr[i];
+        }
+    }
+    printf("\nThe Max In Bills is : %d",Max);
+    getch();
+    return 0;
+}
+
+
+
+
