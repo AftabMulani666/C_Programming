ef027ac4e10517c302e128a278890c60eb824dc0
Aftab Mulani <aftabmulani1739@gmail.com>
2024-10-04 12:31:04 +0530

    008_Array_Min_Of_Array_Elements

---
+++ Array/008_Array_Min_Of_Array_Elements.c
@@ -0,0 +1,31 @@
+
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#define count 8
+
+int main()
+{
+    int Arr[count]={};
+    int i=0,Min=0;
+    for(i=0;i<count;i++)
+    {
+        printf("Enter The Price Of Bill %d :",i);
+        scanf("%d",&Arr[i]);
+    }
+    Min=Arr[0];
+    for(i=0;i<count;i++)
+    {
+        if((Arr[i]<Min) )
+        {
+            Min=Arr[i];
+        }
+    }
+    printf("\nThe Min In Bills is : %d",Min);
+    getch();
+    return 0;
+}
+
+
+
+
