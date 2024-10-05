e1755dd25e9013d2f0c05e78b8ed13b5a497885b
Aftab Mulani <aftabmulani1739@gmail.com>
2024-10-05 17:03:27 +0530

    012_Basic_Array_2

---
+++ Array/012_Basic_Array_2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Value[8]={10,20,30,40,50,60,70,80};
+
+
+    printf("\nThe Value Of 1 Is %d",Value[0]);
+    printf("\nThe Value Of 2 Is %d",Value[1]);
+    printf("\nThe Value Of 3 Is %d",Value[2]);
+    printf("\nThe Value Of 4 Is %d",Value[3]);
+    printf("\nThe Value Of 5 Is %d",Value[4]);
+    printf("\nThe Value Of 6 Is %d",Value[5]);
+    printf("\nThe Value Of 7 Is %d",Value[6]);
+    printf("\nThe Value Of 8 Is %d",Value[7]);
+
+    getch();
+    return 0;
+}
+
