1c607b4b912555f67c50e98b3aa5147529a0f73d
Aftab Mulani <aftabmulani1739@gmail.com>
2024-10-06 16:01:57 +0530

    013_Basic_Array_Char

---
+++ Array/013_Basic_Array_Char.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char Name[10]={'K','A','R','A','D',' ','C','I','T','Y'};
+
+
+    printf("\n%c",Name[0]);
+    printf("\n%c",Name[1]);
+    printf("\n%c",Name[2]);
+    printf("\n%c",Name[3]);
+    printf("\n%c",Name[4]);
+    printf("\n%c",Name[5]);
+    printf("\n%c",Name[6]);
+    printf("\n%c",Name[7]);
+    printf("\n%c",Name[8]);
+    printf("\n%c",Name[9]);
+
+    getch();
+    return 0;
+}
+
+
