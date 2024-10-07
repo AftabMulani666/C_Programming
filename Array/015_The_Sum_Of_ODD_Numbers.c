12c465a0b0e80a8da0c668771c664192f921a17e
Aftab Mulani <aftabmulani1739@gmail.com>
2024-10-07 20:56:48 +0530

    015_The_Sum_Of_ODD_Numbers

---
+++ Array/015_The_Sum_Of_ODD_Numbers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+#define size 10
+int main()
+{
+  int i=0,Arr[size]={},sum=0;
+  for(i=0;i<=size;i++)
+  {
+      printf("Enter The Number :");
+      scanf("%d",&Arr[i]);
+  }
+  for(i=0;i<=size;i++)
+  {
+      if(Arr[i]%2==1)
+      {
+          sum+=Arr[i];
+      }
+  }
+  printf("\nThe Sum Of odd Numbers : %d",sum);
+  getch();
+  return 0;
+}
+
