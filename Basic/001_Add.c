f624004f1a09efae82dc791105604db7c67172c2
Aftab Mulani <aftabmulani1739@gmail.com>
2024-10-12 14:56:40 +0530

    Create 001_Add.c

---
+++ Basic/001_Add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+   int a,b;
+   printf("Enter a Number A\n");
+   scanf("%d",&a);
+   printf("Enter a Number B\n");
+   scanf("%d", &b);
+printf("Your Addition Answer IS %d\n",a+b);
+printf("Your Subtraction Answer IS %d\n",a-b);
+printf("Your Multiplication Answer IS %d\n",a*b);
+printf("Your Division Answer IS %d\n",a/b);
+//Hey This Is a Calculator
+// It Can Provide output Of all Mathematical Operators
+    return 0;
+}
