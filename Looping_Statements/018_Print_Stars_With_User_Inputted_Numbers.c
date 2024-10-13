dcf16194956877773504a614d23b20938063ea24
Aftab Mulani <aftabmulani1739@gmail.com>
2024-10-13 13:42:23 +0530

    018_Print_Stars_With_User_Inputted_Numbers

---
+++ Looping_Statements/018_Print_Stars_With_User_Inputted_Numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i=0,Cnt=0;
+    printf("Enter The Number :");
+    scanf("%d",&Cnt);
+
+    for(i=1;i<=Cnt;i++)
+    {
+        printf(" * ");
+    }
+    printf("**********Thanks************");
+    getch();
+    return 0;
+}
