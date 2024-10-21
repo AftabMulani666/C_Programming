521fb1efd2d90ae06f80d416eb94369761e31016
Aftab Mulani <aftabmulani1739@gmail.com>
2024-10-21 13:32:57 +0530

    016_Display_Array_Of_Bills

---
+++ Array/016_Display_Array_Of_Bills.c
@@ -0,0 +1,27 @@
+
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int i = 0,Bills[7] = {0};
+
+    for(i=0;i<7;i++)
+    {
+        printf("Enter The Bill No %d -> ",i+1);
+        scanf("%d",&Bills[i]);
+    }
+    getch();
+    system("cls");
+
+    printf("Here's The Bills ->");
+
+    for(i=0;i<7;i++)
+    {
+        printf("\nThe Bill No %d is : %d",i+101,Bills[i]);
+    }
+
+    getch();
+    return 0;
+}
