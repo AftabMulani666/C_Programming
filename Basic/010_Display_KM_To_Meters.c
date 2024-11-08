ae185e36f667c8be5c65653dd03a3e315c03e314
Aftab Mulani <aftabmulani1739@gmail.com>
2024-11-08 21:18:00 +0530

    Create 010_Display_KM_To_Meters.c

---
+++ Basic/010_Display_KM_To_Meters.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    float kilometers, meters;
+
+    printf("Enter distance in kilometers: ");
+    scanf("%f", &kilometers);
+
+    
+    meters = kilometers * 1000;
+
+    
+    printf("Distance in meters: %.2f meters\n", meters);
+
+    return 0;
+}
