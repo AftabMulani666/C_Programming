b8a9c898d4e29cde3c707d93f5d7ec92bd2a02d9
Aftab Mulani <aftabmulani1739@gmail.com>
2024-12-06 00:49:37 +0530

    Create 011_Student_details.c

---
+++ Basic/011_Student_details.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    char name[50], course[50], city[50];
+    int rollNumber;
+
+    // Accepting student details from the user
+    printf("Enter Student Name: ");
+    scanf("%49[^\n]", name); // Read input until a newline
+    getchar(); // To consume the newline left in the input buffer
+
+    printf("Enter Roll Number: ");
+    scanf("%d", &rollNumber);
+    getchar(); // To consume the newline left in the input buffer
+
+    printf("Enter Course: ");
+    scanf("%49[^\n]", course);
+    getchar(); // To consume the newline left in the input buffer
+
+    printf("Enter City: ");
+    scanf("%49[^\n]", city);
+
+    // Displaying all student information in a single print statement
+    printf("Student Name: %s, Roll Number: %d, Course: %s, City: %s\n", name, rollNumber, course, city);
+
+    return 0;
+}
